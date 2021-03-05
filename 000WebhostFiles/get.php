8ebdaf3ad4765b2b37d8472a3322aa0d41728bf4
Jxnahs / Jonah#1337 <74617956+Jxnahs@users.noreply.github.com>
2021-03-05 02:02:13 +0000

    Update get.php

--- 000WebhostFiles/get.php
+++ 000WebhostFiles/get.php
@@ -1,1 +1,16 @@
+<?php
 
+$database = $_GET['database'];
+
+$key = $_GET['key'];
+
+$filename = "Datastores/$database/$key";
+if (file_exists($filename)) {
+    $handle = fopen($filename, "r");
+    $contents = fread($handle, filesize($filename));
+    echo $contents;
+    fclose($handle);
+} else {
+    echo "No data";
+}
+?>
